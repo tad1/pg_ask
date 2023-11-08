8f8e9c2736e8db57eb3f9fa466c073d7596af2eb
tad1 <tadziobrzeski@gmail.com>
2023-11-08 18:53:10 +0100

    remove painfull 2 minute awaiting

--- lab3/init.sh
+++ lab3/init.sh
@@ -6,3 +6,5 @@ systemctl start sshd
 
 sudo apt install isc-dhcp-server
 sudo systemctl disable isc-dhcp-server.service
+
+cp ./systemd-networkd-wait-online.service /etc/systemd/system/network-online.target.wants/
