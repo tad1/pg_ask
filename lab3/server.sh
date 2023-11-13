472bb4eb6476162e2bf34686abc8b8a2d5ceb283
tad1 <tadziobrzeski@gmail.com>
2023-11-13 11:35:48 +0100

    set default server IP configuration

--- lab3/server.sh
+++ lab3/server.sh
@@ -1,11 +1,10 @@
 # 0. clear
-ip addr del 10.0.0.1/24 dev enp0s3
+# ip addr del 10.0.0.1/24 dev enp0s3
 systemctl stop sshd
 apt remove sshd
 
 # 1. copy configs
 cp ~/dhcpd.conf /etc/default/isc=dhcp-server/
-cp ./server_netplan.yaml /etc/netplan/
 
 # 2. enable dhcp
 systemctl enable isc-dhcp-server.service
--- lab3/server_ip.sh
+++ lab3/server_ip.sh
@@ -1,1 +1,3 @@
-sudo ip addr add 10.0.0.1/24 dev enp0s3
+# sudo ip addr add 10.0.0.1/24 dev enp0s3
+cp ./server_netplan.yaml /etc/netplan/
+sudo netplan apply
